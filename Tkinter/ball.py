cec516f9e519e10035a9b2e3b73c9830183383c9
dichenko <m@dichenko.ru>
2016-07-25 10:14:04 +0300

    задача соединить точки

--- Tkinter/ball.py
+++ Tkinter/ball.py
@@ -2,16 +2,21 @@ import tkinter as t
 root = t.Tk()
 
 def fu(event):
-
     me = event.widget
     if me == button1:
         print('b1')
 
+def fur():
+    print('ololo')
 
-button1 = t.Button(root, text = "Йя кнопко")
-button1.bind("<Button>", fu)  #<Button-1> левая кнопка мыши, 3-правая
+#способ 1 - не биндим, используем command. Кнопка срабатывает как кнопка, с пробела, и по отпусканию кнопки мыши
+button1 = t.Button(root, text = "Йя кнопко", command=fur)
+#button1.bind("<Button>", fu)  #<Button-1> левая кнопка мыши, 3-правая
 button1.pack()
 
+
+#Способ 2 - забиндить кнопку. Срабатывает на нажатие (а не на отпускание) кнопки мыши
+#и не нажимается пробелом
 button2 = t.Button(root, text = "Йя попко")
 button2.bind("<Button>", fu)  #<Button> любой кнопкой мыши
 button2.pack()
