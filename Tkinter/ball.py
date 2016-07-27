1cb5742b37d479d38c3d39a254ac7b23bc4578f3
dichenko <m@dichenko.ru>
2016-07-27 15:20:35 +0300

    Добавили виджетов в окошко

--- Tkinter/ball.py
+++ Tkinter/ball.py
@@ -1,5 +1,5 @@
 import tkinter as t
-root = t.Tk()
+
 
 def fu(event):
     me = event.widget
@@ -9,20 +9,30 @@ def fu(event):
 def fur():
     print('ololo')
 
-#способ 1 - не биндим, используем command. Кнопка срабатывает как кнопка, с пробела, и по отпусканию кнопки мыши
-button1 = t.Button(root, text = "Йя кнопко", command=fur)
-#button1.bind("<Button>", fu)  #<Button-1> левая кнопка мыши, 3-правая
-button1.pack()
+
+def init_main_window():
+    global root, button1, button2, label, text, ruler
+    root = t.Tk()
+
+    #способ 1 - не биндим, используем command. Кнопка срабатывает как кнопка, с пробела, и по отпусканию кнопки мыши
+    button1 = t.Button(root, text = "Йя кнопко", command=fur)
+    #button1.bind("<Button>", fu)  #<Button-1> левая кнопка мыши, 3-правая
 
 
-#Способ 2 - забиндить кнопку. Срабатывает на нажатие (а не на отпускание) кнопки мыши
-#и не нажимается пробелом
-button2 = t.Button(root, text = "Йя попко")
-button2.bind("<Button>", fu)  #<Button> любой кнопкой мыши
-button2.pack()
+    #Способ 2 - забиндить кнопку. Срабатывает на нажатие (а не на отпускание) кнопки мыши
+    #и не нажимается пробелом
+    button2 = t.Button(root, text = "Йя попко")
+    button2.bind("<Button>", fu)  #<Button> любой кнопкой мыши
+    variable = t.IntVar()
+    label = t.Label(root, text = 'Тут лейбл')
+    ruler = t.Scale(root)
+    text = t.Entry(root)
+    for obj in button1, button2, label, text, ruler:
+        obj.pack()
 
 
+if __name__ == "__main__":
+    global root
 
-button1['text'] = 'Я текст'
-
-root.mainloop()
+    init_main_window()
+    root.mainloop()
