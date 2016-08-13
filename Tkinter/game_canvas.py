b0518f4599cb2062469f9d0129ab0663b1af7f63
dichenko <m@dichenko.ru>
2016-08-13 11:56:41 +0300

    двигаем линию по холсту

--- Tkinter/game_canvas.py
+++ Tkinter/game_canvas.py
@@ -1,7 +1,5 @@
 import tkinter
 
-
-
 def paint(event):
     """ Обработчик событий для холста
     :param event: событие
@@ -11,12 +9,16 @@ def paint(event):
     canvas.coords(line, 0,0,event.x, event.y)
 
 root = tkinter.Tk()
-canvas = tkinter.Canvas(root)
+canvas = tkinter.Canvas(root, background='yellow', width=400, height=400)
 canvas.bind("<Motion>", paint)
 canvas.pack()
 
+for i in range(10):
+    canvas.create_oval(i*40, i*40, i*40+30, i*40+30, fill='green', width='0')
 
 line = canvas.create_line(0,0,10,100)
+
+
 
 
 
---
+++ Tkinter/wigets_testing.py
@@ -0,0 +1,37 @@
+import tkinter as t
+
+
+def fu(event):
+    me = event.widget
+    if me == button1:
+        print('b1')
+
+def fur():
+    print('ololo')
+
+
+def init_main_window():
+    global root, button1, button2, label, text, ruler
+    root = t.Tk()
+
+    #способ 1 - не биндим, используем command. Кнопка срабатывает как кнопка, с пробела, и по отпусканию кнопки мыши
+    button1 = t.Button(root, text = "Йя кнопко", command=fur)
+    #button1.bind("<Button>", fu)  #<Button-1> левая кнопка мыши, 3-правая
+
+
+    # Способ 2 - забиндить кнопку. Срабатывает на нажатие
+    # (а не на отпускание) кнопки мыши
+    # и не нажимается пробелом
+    button2 = t.Button(root, text = "Йя попко")
+    button2.bind("<Button>", fu)  #<Button> любой кнопкой мыши
+    variable = t.IntVar()
+    label = t.Label(root, textvariable = variable)
+    ruler = t.Scale(root, orient=t.HORIZONTAL, from_=0, to=255, resolution=10, variable=variable)
+    text = t.Entry(root, textvariable = variable)
+    for obj in button1, button2, label, text, ruler:
+        obj.pack()
+
+
+if __name__ == "__main__":
+    init_main_window()
+    root.mainloop()
