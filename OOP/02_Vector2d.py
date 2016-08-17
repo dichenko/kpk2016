f18b8f3e4bfe7eb0ec207bdd9bd66aee1800b92a
dichenko <m@dichenko.ru>
2016-08-17 23:17:40 +0300

    ООП 2

--- OOP/02_Vector2d.py
+++ OOP/02_Vector2d.py
@@ -3,6 +3,17 @@ class Vector2D():
         self._x = x
         self._y = y
     def plus(self,other):
-        x = self._x + other.x
-        y = self._y + other.y
+        x = self._x + other._x
+        y = self._y + other._y
         return Vector2D(x,y)
+    def __str__(self):
+        """
+        :return: #отвечает за то что ббудет выведено на печать при печати экземпляра класса
+        """
+        return '('+str(self._x)+'; '+str(self._y)+')'
+
+a = Vector2D(2,2)
+b = Vector2D(-1,3)
+c = a.plus(b)
+
+print(a, '+', b, '=',c)
