acf914e59ad1c50abcd66180af274c038913f561
dichenko <m@dichenko.ru>
2016-08-19 20:38:31 +0300

    gun 1

--- OOP/02_Vector2d.py
+++ OOP/02_Vector2d.py
@@ -11,14 +11,21 @@ class Vector2D():
         x = self._x + other._x
         y = self._y + other._y
         return Vector2D(x,y)
+
     def __str__(self):
         """Переопределяем Print для класса
         :return: #отвечает за то что ббудет выведено на печать при печати экземпляра класса
         """
         return '(%d, %d)'%(self._x, self._y)
 
+    def __mul__(self,other):
+        """Переопределяем умножение для класса"""
+        x = self._x * other._x
+        y = self._y * other._y
+        return Vector2D(x+y)
+
 a = Vector2D(2,2)
 b = Vector2D(-1,3)
-c = a + b
+c = a * b
 
-print(a, '+', b, '=',c)
+print(a, '*', b, '=',c)
---
+++ OOP/gun.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from random import choice, randint
+
+ball_initial_number = 20
+ball_minimum_radius = 15
+ball_maximum_radius = 50
+ball_available_colors = ['green','blue','red','yellow','grey','#FF0000','#00FF00','#0000FF']
+
+def init_game():
+    """
+    Создаем шарики и пушку
+    :return:
+    """
+
+def inint_main_window():
+    global root, canvas, score_text, score_value
+    root = Tk()
+    #FIXME
+
+if __name__ == "__main__":
+    inint_main_window()
+    init_game()
+    root.mainloop()
