83997a6d468cf00c822ca7a6cc567432916536ad
dichenko <m@dichenko.ru>
2016-08-19 11:05:26 +0300

    ООП 2

--- OOP/02_Vector2d.py
+++ OOP/02_Vector2d.py
@@ -2,18 +2,23 @@ class Vector2D():
     def __init__(self,x=0,y=0):
         self._x = x
         self._y = y
-    def plus(self,other):
+    def __add__(self,other):
+        '''
+        #Переопределяем стандартный метод, чтобы сложение векторов
+        давало нам именно то что мы хотим
+        :return: метод отвечает за операнд "+", складывая координаты.
+        '''
         x = self._x + other._x
         y = self._y + other._y
         return Vector2D(x,y)
     def __str__(self):
-        """
+        """Переопределяем Print для класса
         :return: #отвечает за то что ббудет выведено на печать при печати экземпляра класса
         """
-        return '('+str(self._x)+'; '+str(self._y)+')'
+        return '(%d, %d)'%(self._x, self._y)
 
 a = Vector2D(2,2)
 b = Vector2D(-1,3)
-c = a.plus(b)
+c = a + b
 
 print(a, '+', b, '=',c)
