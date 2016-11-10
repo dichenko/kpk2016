01b6da3b9b5f7d9eb27d2c7e5954d6512c2d4de8
dichenko <m@dichenko.ru>
2016-11-10 17:49:44 +0300

    nti

--- Diff/olymp_school/nti_6.py
+++ Diff/olymp_school/nti_6.py
@@ -4,3 +4,13 @@ for i in range(n):
     m.append(list(map(int, input().split())))
 
 u = [0,0,0]
+
+u[0] = m[0][1]
+u[1] = m[0][2]
+u[2] = m[0][3]
+
+print(*u)
+
+for i in range(1,n):
+    pass
+    #FIXME!!!
