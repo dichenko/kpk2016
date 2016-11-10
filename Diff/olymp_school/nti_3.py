6fa6c694d7d128d161c30d4fe799af8b88f2a569
dichenko <m@dichenko.ru>
2016-11-10 11:51:55 +0300

    ee

--- Diff/olymp_school/nti_3.py
+++ Diff/olymp_school/nti_3.py
@@ -2,13 +2,10 @@ rate = float(input())
 power = int(input())
 a_std1 = "7E7E7E7E7E7E7E7E"
 a_std2 = "1E1E"
-
 rate = str(hex(int((rate-422.4)*10)))[2:]
-
 if power == -10: power = "00"
 elif power == -2: power = "01"
 elif power == 6: power = "02"
 elif power == 10: power = "03"
-
-print(a_std1+  rate +       power+     a_std2)
+print(a_std1+rate+power+a_std2)
 
